330cd68e912d7f841f44c91a74008d6a0d24a8d0
badrinadh12 <badrinadhgoru@gmail.com>
2026-08-12 19:38:01 +0530

    LC-palindrome-number [Runtime: N/A; Memory: N/A]

--- math/palindrome-number.c
+++ math/palindrome-number.c
@@ -1,10 +1,11 @@
 
 bool isPalindrome(int x) {
-    if(x==0){
-        return true;
+    if(x==0||(x<0)){
+        return false;
     }
     int p=0;
     int original=x;
+    
     while(x>0){
         p=p*10 + (x%10);
         x=x/10;
